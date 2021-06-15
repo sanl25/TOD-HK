19df5110b7271ba99c8733287ee0bee14dcdfdf3
wingtat.yau <wingtat.yau@gmail.com>
2021-06-15 13:31:21 +0800

    Updated URL

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mapbox-gl-draw-circle-test/precache-manifest.c4149aeeae43c7d678634cbf930c59e7.js"
+  "/tod-hk/precache-manifest.c4149aeeae43c7d678634cbf930c59e7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mapbox-gl-draw-circle-test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tod-hk/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
